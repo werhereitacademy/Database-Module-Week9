2ecc005175f1746661c1743634ec9b68570e56d4
Mustafa HAYBAT <mustaphahaybat@gmail.com>
2025-06-20 00:53:33 +0200

    Update Mustafa.sql

--- Mustafa.sql
+++ Mustafa.sql
@@ -1,35 +1,56 @@
---Show the first and last names of actors whose first names are Nick, Ed, or Jennifer.
+-- 1-
+-- SELECT first_name, last_name 
+-- FROM public.actor
+-- WHERE first_name IN('Ed', 'Nick', 'Jennifer'); 
+
+-- 2-
+-- SELECT last_name
+-- FROM actor
+-- WHERE first_name IN('Ed', 'Nick', 'Jennifer'); 
+
+-- 3-
+-- SELECT * FROM address;
+
+-- 4-
+-- SELECT district, phone FROM address
+-- ORDER BY phone DESC;
+
+-- 5-
+-- SELECT film.film_id, film.title, inventory.inventory_id
+-- FROM film JOIN inventory
+-- ON film.film_id = inventory.film_id;
+
+-- 6-
+-- SELECT * 
+-- FROM inventory INNER JOIN rental
+-- ON inventory.inventory_id = rental.inventory_id
+-- LIMIT 5;
+
+-- 7-
+-- SELECT rental.rental_id, rental.rental_date, payment.payment_id
+-- FROM rental JOIN payment 
+-- ON rental.rental_id = payment.rental_id
+-- ORDER BY payment.amount DESC
+-- LIMIT 10;
+
+-- 8-
+-- SELECT * FROM actor
+-- WHERE actor_id IS NULL;
+
+-- 9-
+-- SELECT * FROM actor
+-- WHERE actor_id IS NOT NULL;
+
+-- 10-
+-- SELECT COUNT(*) FROM film;
+
+-- 11-
+-- SELECT SUM(amount) AS sum_amt FROM payment;
+
+-- 12-
+-- SELECT MAX(amount) AS Maximum, MIN(amount) AS Minumum FROM payment;
 
 
---Show only the last name of actors whose first names are Ed, Nick, or Jennifer.
 
-
---Show all details from the address table..
-
-
---Display district and phone from the address table, sorted in descending order.
-
-
---Using film_id from the film and inventory tables, show film_id, title from the film table, and inventory_id from the inventory table.
-
-
---Show the first 5 rows consisting of the inventory and rental tables.
-
-
---Show the first 10 rows consisting of rental_id, rental_date, and payment_id from the rental and payment tables, ordered by amount in descending order.
-
-
---Show all details of rows where actor_id is empty (NULL) in the actor table.
-
-
---Show all details of rows in the actor table where actor_id is not empty (NOT NULL).
-
---Find the number of non-empty rows in the film table.
-
-
---Show the total amount from the payment table as the output header sum_amt.
-
-
---Show the maximum and minimum amount in the payment table.
 
 
